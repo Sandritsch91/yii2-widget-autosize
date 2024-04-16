09bfa98ae2d929d7318a739c6a24dfbba32f0d6d
Sandritsch91 <sandritsch91@gmail.com>
2024-04-16 13:35:05 +0200

    Bugfixes and improvements

---
+++ README.md
@@ -0,0 +1,71 @@
+# yii2-widget-autosize
+
+An [autosize](https://www.jacklmoore.com/autosize/) widget for [yii2](https://www.yiiframework.com/)
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```bash
+php composer.phar require --prefer-dist sandritsch91/yii2-widget-autosize
+```
+
+or add
+
+```json
+"sandritsch91/yii2-widget-autosize": "*"
+```
+
+to the require section of your composer.json
+
+## Usage
+
+with a model:
+
+```php
+use sandritsch91\yii2-widget-autosize\Autosize;
+
+echo Autosize::widget([
+    'model' => $model,                          // The model to be used in the form
+    'attribute' => 'content',                   // The attribute to be used in the form
+    'htmlClass' => yii\bootstrap5\Html::class,  // Optional. The class used to generate the form field
+    'clientEvents' => [                         // Optional. Pass the client events to be attached to the textarea
+        'autosize:resized' => 'function() { console.log("resized"); }'
+    ]
+]);
+```
+
+with an ActiveForm:
+
+```php
+use sandritsch91\yii2-widget-autosize\Autosize;
+
+echo $form->field($model, 'content')->widget(Autosize::class, [
+    'clientEvents' => [
+        'autosize:resized' => 'function() { console.log("resized"); }'
+    ]
+])
+```
+
+without a model:
+
+```php
+use sandritsch91\yii2-widget-autosize\Autosize;
+
+echo Autosize::widget([
+    'name' => 'myText',         // The name of the input
+    'value' => 'Hello World',   // The value of the input
+    'clientEvents' => [
+        'autosize:resized' => 'function() { console.log("resized"); }'
+    ]
+]);
+```
+
+## Widget options
+- clientEvents: The client events to be attached to the textarea. Defaults to []
+  - autosize:resized: Triggered when the textarea is resized
+  - autosize:update: Dispatch this event to update the textarea. No event is triggered by the plugin
+  - autosize:destroy: Dispatch this event to destroy the textarea. No event is triggered by the plugin
+- htmlClass: The class used to generate the form field. Defaults to yii\helpers\Html::class
--- src/Autosize.php
+++ src/Autosize.php
@@ -8,14 +8,11 @@ use yii\widgets\InputWidget;
 class Autosize extends InputWidget
 {
     /**
-     * The Html class to use.
+     * The class used to generate the form field.
      * @var string|Html
      */
     public string|Html $htmlClass = 'yii\helpers\Html';
-    /**
-     * @var array the options for the underlying JS plugin.
-     */
-    public array $clientOptions = [];
+
     /**
      * @var array the event handlers for the underlying JS plugin.
      */
@@ -26,29 +23,35 @@ class Autosize extends InputWidget
      */
     public function run(): string
     {
-        $this->registerClientScript();
+        if ($this->hasModel()) {
+            $id = $this->options['id'] ?? $this->htmlClass::getInputId($this->model, $this->attribute);
+            $html = $this->htmlClass::activeTextarea($this->model, $this->attribute, $this->options);
+        } else {
+            $id = $this->options['id'] ?? $this->getId();
+            $html = $this->htmlClass::textarea($this->name, $this->value, $this->options);
+        }
 
-        return ($this->hasModel())
-            ? $this->htmlClass::activeInput('text', $this->model, $this->attribute, $this->options)
-            : $this->htmlClass::input('text', $this->name, $this->value, $this->options);
+        $this->registerClientScript($id);
+
+        return $html;
     }
 
     /**
      * Registers the needed JavaScript.
      */
-    protected function registerClientScript(): void
+    protected function registerClientScript(string $id): void
     {
         $view = $this->getView();
         AssetBundle::register($view);
 
-        $js = "autosize(document.getElementById('#$this->id', $this->clientOptions));";
+        $js = "autosize(document.getElementById('$id'));";
         $view->registerJs($js);
 
         // Register client events
         if (!empty($this->clientEvents)) {
             $js = [];
             foreach ($this->clientEvents as $event => $handler) {
-                $js[] = "document.getElementById('#$this->id').addEventListener('$event', $handler);";
+                $js[] = "document.getElementById('$id').addEventListener('$event', $handler);";
             }
             $view->registerJs(implode("\n", $js));
         }
